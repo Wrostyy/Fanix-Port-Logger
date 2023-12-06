feeb6fda54cb1ba1e7ba3d3703d9bc3fed57cbaf
Wrosty <69653412+Wrostyy@users.noreply.github.com>
2023-12-06 18:45:55 +0300

    Add files via upload

---
+++ Port Logger/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace UdemyEgitim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Location = new System.Drawing.Point(601, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Active listening port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(534, 316);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(716, 393);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fanix Port Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+    }
+}
---
+++ Port Logger/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdemyEgitim
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener tcpListener;
+        private bool isListening = false;
+
+        private Thread listenerThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int port))
+            {
+                if (port > 65535)
+                {
+                    MessageBox.Show("Invalid port. Port value cannot be greater than 65535.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    listBox1.Items.Add($"Port {port} is listening...");
+                    textBox1.Enabled = false;
+                    label3.Text = port.ToString();
+                    button1.Enabled = false;
+                    listBox1.HorizontalScrollbar = true;
+                    listBox1.Visible = true;
+                    button2.Enabled = true;
+
+                    StartListening(port);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid port number, please enter a number.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopListening();
+        }
+
+        private void StartListening(int port)
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, port);
+                listenerThread = new Thread(new ThreadStart(ListenForClients));
+                listenerThread.Start();
+                isListening = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void StopListening()
+        {
+            if (tcpListener != null)
+            {
+                isListening = false;
+                tcpListener.Stop();
+                listBox1.Items.Add("Port listening has been stopped.");
+                textBox1.Enabled = true;
+                button2.Enabled = false;
+                button1.Enabled = true;
+                label3.Text = "";
+            }
+        }
+
+        private void ListenForClients()
+        {
+            try
+            {
+                tcpListener?.Start();
+
+                while (isListening)
+                {
+                    if (tcpListener != null)
+                    {
+
+                        TcpClient client = tcpListener.AcceptTcpClient();
+
+                        if (client != null)
+                        {
+                            Thread clientThread = new Thread(() => HandleClientComm(client));
+                            clientThread.Start();
+                        }
+                    }
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bağlantı hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void HandleClientComm(TcpClient client)
+        {
+
+            IPEndPoint remoteEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+            string clientIPAddress = remoteEndPoint.Address.ToString();
+
+            DateTime connectionTime = DateTime.Now;
+
+            string clientMachineName;
+            try
+            {
+                clientMachineName = Dns.GetHostEntry(clientIPAddress).HostName;
+            }
+            catch (Exception)
+            {
+                clientMachineName = "Unknown";
+            }
+
+            listBox1.Invoke((MethodInvoker)delegate
+            {
+                listBox1.Items.Add($"'{clientMachineName}' The client named is connected. '{clientIPAddress}' | {connectionTime:yyyy-MM-dd HH:mm}");
+            });
+
+
+            client.Close();
+
+            listBox1.Invoke((MethodInvoker)delegate
+            {
+                listBox1.Items.Add($"'{clientMachineName}' The client named has logged out. He disconnected.'{clientIPAddress}' | {DateTime.Now:yyyy-MM-dd HH:mm}");
+            });
+        }
+
+
+    }
+}
